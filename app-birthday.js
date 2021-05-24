ab49c0636246e8c9f46d2beebae3cb43d3d21aa2
efe applications <efe.applications@gmail.com>
2021-05-24 23:32:48 +0200

    new

---
+++ app-birthday.js
@@ -0,0 +1,48 @@
+
+
+
+const countdown = () => {
+    const countDate = new Date("July 8, 2021 00:00:00").getTime();
+
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector(".day").innerHTML = textDay;
+    document.querySelector(".hour").innerHTML = textHour;
+    document.querySelector(".minute").innerHTML = textMinute;
+    document.querySelector(".second").innerHTML = textSecond;
+
+
+    
+    
+    if (textDay == 0 &&
+        textHour == 0 &&
+        textMinute == 0 &&
+        textSecond == 0) {
+            document.querySelector(".title").innerHTML = "Happy Birthday Trucho!"; 
+            myStopFunction();
+        }
+
+};
+
+const myInterval = setInterval(countdown, 1000);
+
+function myStopFunction() {
+    clearInterval(myInterval);
+  }
+
+
+
+
+
+
